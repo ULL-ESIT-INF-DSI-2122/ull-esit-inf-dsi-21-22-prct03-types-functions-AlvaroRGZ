76db9343398a164e2e4d89fa2fe1377aab381a50
AlvaroRGZ <alu0101362953@ull.edu.es>
2022-02-26 12:12:49 +0000

    Ejer 2 preparado

---
+++ src/ejercicio-2.ts
@@ -0,0 +1,15 @@
+console.log('EJERCICIO 2) Notacion decimal y factorial');
+
+function decimalToFactorial(Year: number): string {
+  const a: string = 'hola';
+  return a;
+}
+
+function factorialToDecimal(Year: number): string {
+  const a: string = 'hola';
+  return a;
+}
+
+console.log('Pruebas:');
+console.log('463: ', decimalToFactorial(463), ' vs 341010');
+console.log('341010: ', factorialToDecimal(341010), ' vs 463');
