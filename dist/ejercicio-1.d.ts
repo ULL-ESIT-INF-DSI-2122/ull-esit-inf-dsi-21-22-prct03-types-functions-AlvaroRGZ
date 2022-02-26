37c2d359c04b8a40b205a286e9d715b242216de1
AlvaroRGZ <alu0101362953@ull.edu.es>
2022-02-26 11:55:47 +0000

    ejer 1 - fin

---
+++ dist/ejercicio-1.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Returns true or false if the year is leap or not.
+ * @param {int} Year year.
+ * @return {boolean} If the year is leap or not.
+ */
+declare function isLeapYear(Year: number): boolean;
--- dist/ejercicio-1.js
+++ dist/ejercicio-1.js
@@ -1,2 +1,18 @@
 "use strict";
 console.log('EJERCICIO 1) AÑOS BISIESTOS');
+/**
+ * Returns true or false if the year is leap or not.
+ * @param {int} Year year.
+ * @return {boolean} If the year is leap or not.
+ */
+function isLeapYear(Year) {
+    if ((Year % 4 == 0) && ((Year % 100 != 0) || (Year % 400 == 0))) {
+        return true;
+    }
+    return false;
+}
+console.log('Pruebas:');
+console.log('1997: ', isLeapYear(1997));
+console.log('1996: ', isLeapYear(1996));
+console.log('1900: ', isLeapYear(1900));
+console.log('2000: ', isLeapYear(2000));
